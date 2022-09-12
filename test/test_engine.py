fab7ae91fd392c87806fb46e02924e15b4cb9203
shandlermason <samason1@aggies.ncat.edu>
2022-09-11 23:37:47 -0400

    implemented test script that runs all test

--- test/test_engine.py
+++ test/test_engine.py
@@ -2,12 +2,12 @@ from code.num import Num
 from code.settings import oo, the
 
 
+# test if object exist
 def test_the():
-    oo(the)
     return True
 
 
-def test_num(mid, div):
+def test_num():
     num = Num()
     for i in range(0, 100):
         num.add(i)
@@ -15,3 +15,14 @@ def test_num(mid, div):
     print(mid, div)
     assert (50 <= mid <= 52) and (30.5 < div < 32)
 
+
+# executes each test and stores results at the end prints results and # of fails
+def main():
+    fail_count = 0
+    fail_count += test_the()
+    fail_count += test_num()
+    return fail_count
+
+
+if __name__ == "__main__":
+    main()
