165bd819793691f4b4d7bc15f0fce7d6b4f8e7e1
shandlermason <samason1@aggies.ncat.edu>
2022-09-12 13:13:53 -0400

    sym mid and div method

--- src/sym.py
+++ src/sym.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Sym:
     def __init__(self, c=0, s=""):
         self.n = 0
@@ -13,3 +16,21 @@ class Sym:
                 self._has[v] += 1
             else:
                 self._has[v] = 1
+
+    # mode - most common symbol
+    def mid(self, col):
+        most = -1
+        for k, v in self._has:
+            if v > most:
+                mode, most = k, v
+        return mode
+
+    # entropy - symbols occurring at probability
+    def div(self):
+        def fun(p):
+            return p * math.log(p, 2)
+        e = 0
+        for n in self._has.values():
+            if n > 0:
+                e = e - fun(n/self.n)
+        return e
