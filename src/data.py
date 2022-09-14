6c9bfae779bc974ee0b407c454f0ef4fdebd4fb9
shandlermason <samason1@aggies.ncat.edu>
2022-09-13 21:30:30 -0400

    still trying to figure out data add class

--- src/data.py
+++ src/data.py
@@ -3,7 +3,7 @@ from src import cols
 from src import settings
 from src.row import Row
 from src.cols import Cols
-
+from _collections import defaultdict
 
 class Data:
     def __init__(self, src):
@@ -45,9 +45,30 @@ class Data:
     def add(self, xs):
         if self.cols is None:
             self.cols = Cols(xs[0])
+            self.rows.pop(0)
+            # remove list 1 from overLL LIST
+
         print(self.cols)
+        e = self.rows[0][0]
+        nr = defaultdict(list)
+        dd = defaultdict(list)
+
+        i = 0
+        for n in self.rows:
+            i = 0
+            for x in n:
+                nr[i].append(x)
+                i += 1
+
+        nr[0] = e
+
+        for d in (self.cols.x, self.cols.y, nr):
+            for k, v in d.items():
+                dd[k].append(v)
 
 
+
+        print('d', dd.keys())
 
         """
         else:
